2ec0867c98595f64c144ca1269daf2cf345dbcab
Gabriel Garcia <sofaltaumaletr@gmail.com>
2026-08-26 14:48:28 -0300

    feat: ocultar barra de status no Android

--- src-tauri/gen/android/app/src/main/java/com/controle_gastos/app/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/controle_gastos/app/MainActivity.kt
@@ -2,10 +2,19 @@ package com.controle_gastos.app
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+    WindowInsetsControllerCompat(window, window.decorView).apply {
+      hide(WindowInsetsCompat.Type.statusBars())
+      systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
   }
 }
